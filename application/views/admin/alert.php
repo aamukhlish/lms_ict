d04df4fa183bcd66d39c28ed9aad98086f33072d
aamukhlish <43819792+aamukhlish@users.noreply.github.com>
2019-05-20 11:53:19 +0700

    halaman severity

--- application/views/admin/alert.php
+++ application/views/admin/alert.php
@@ -14,13 +14,9 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Dashboard
-        <small>Version 2.0</small>
+        Severity
+        <small>Alert</small>
       </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Dashboard</li>
-      </ol>
     </section>
     
     <section class="content">
---
+++ application/views/admin/notif.php
@@ -0,0 +1,30 @@
+<?php if(!$this->session->userdata['login']){redirect(base_url('login'));} ?>
+<!DOCTYPE html>
+<html>
+  <?php $this->load->view('admin/head') ?>
+<body class="hold-transition skin-purple sidebar-mini">
+<div class="wrapper">
+
+  <?php $this->load->view('admin/header') ?>
+  <!-- Left side column. contains the logo and sidebar -->
+  <?php $this->load->view('admin/leftbar') ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Severity
+        <small>Notification</small>
+      </h1>
+    </section>
+    
+    <section class="content">
+    </section>
+    
+  </div>
+  <!-- /.content-wrapper -->
+
+  <?php $this->load->view('admin/footer') ?>
+</body>
+</html>
